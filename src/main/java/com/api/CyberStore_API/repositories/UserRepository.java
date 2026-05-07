d084b9c48b820fb77773ae28a31c777878d7d2f7
igorleite97 <igorleite2018@gmail.com>
2026-05-07 20:14:03 -0300

    feat: implement JPA repository and database seeding with H2

---
+++ src/main/java/com/api/CyberStore_API/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.api.CyberStore_API.repositories;
+
+import com.api.CyberStore_API.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+
+
+
+}
+
