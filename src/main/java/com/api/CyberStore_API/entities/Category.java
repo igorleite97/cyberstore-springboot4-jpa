e617ded04eab1f04b45d7452821692296bcbbd4e
igorleite97 <igorleite2018@gmail.com>
2026-05-11 20:54:23 -0300

    Seed database with Categories, Products and Associations

--- src/main/java/com/api/CyberStore_API/entities/Category.java
+++ src/main/java/com/api/CyberStore_API/entities/Category.java
@@ -1,20 +1,29 @@
 package com.api.CyberStore_API.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
+import java.io.Serial;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
 public class Category implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -51,4 +60,9 @@ public class Category implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
 }
---
+++ src/main/java/com/api/CyberStore_API/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.api.CyberStore_API.repositories;
+
+import com.api.CyberStore_API.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    
+
+
+
+}
+
